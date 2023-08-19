9b8ee49061b9080a04d7a18e52128f724d1087b7
AadityanshuSingh <125582586+AadityanshuSingh@users.noreply.github.com>
2023-08-19 19:49:45 +0530

    Fifth Commit

--- components/Data.jsx
+++ components/Data.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import useFetch from './FetchApi'
-import { Card, Heading } from '@chakra-ui/react'
+import { Box, Card, CardBody, Heading } from '@chakra-ui/react'
 
 const Data = () => {
+    const {data, loading, error} = useFetch(`http://api.weatherapi.com/v1/current.json?key=0192de40d23c418d8fc140232231908&q=London&aqi=no`)
   return (
     <>
         <Card m={4}>
             <Heading>
                 Weather
             </Heading>
+            <CardBody>
+                <Box>
+                    {data.name}
+                </Box>
+            </CardBody>
+
         </Card>
     </>
   )
